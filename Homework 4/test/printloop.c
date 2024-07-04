cbfe184cd78431f7880dc60e2f01312d0f862893
Riccardo Massi <52914355+rickywar@users.noreply.github.com>
2024-07-04 17:42:34 +0200

    Update printloop.c

--- Homework 4/test/printloop.c
+++ Homework 4/test/printloop.c
@@ -6,17 +6,13 @@ int main() {
 	int A[10];
 	int B[10];
 	int C[10];
-	int a = 0;
-	int b = 0;
 	for(int i = 0; i<n;i++) {
-		a++;
 		A[i]=a;
 		printf("I: %d\n",A[i]);
 	
 	}
 	
 	for(int j = 0; j<n;j++){
-		b+=2;
 		B[j]=b;
 		printf("J: %d\n",B[j]);
 }
